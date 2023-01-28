ce4182babaa7c9805e82a4b2f4c23ccfc2620a7d
David Ospina <J_do_o13@hotmail.com>
2023-01-28 16:28:09 -0500

    Password FUnction
    se añadió la función para geenarr contraseñas aleatorias con 4 letras y 4 números

--- js/Back.js
+++ js/Back.js
@@ -18,7 +18,7 @@ var app = new Vue({
                     LastN: this.LastN,
                     FullName: (this.Name+' '+this.LastN),
                     edad: this.Calc_Age(),
-                    Password: this.Generate(),
+                    Password: this.Generate(0,0),
                     User: this.User,
                 })
                 this.Message = `${this.Users[0].User}`
@@ -51,8 +51,32 @@ var app = new Vue({
             }
             return age
         },
-        Generate(){
-            return 123
+        Generate(letter, number){
+            let aux=0;
+            let str="";
+            while(str.length<8){
+                let rand = Math.floor(Math.random() * 62);
+                aux=rand+48
+                if(rand>9){
+                    aux+=7
+                }
+                if(rand>35){
+                    aux+=6
+                }
+                //console.log(index+'-'+a+'-'+String.fromCharCode(a))
+                if(aux>=48 && aux<58 && number<4){
+                    console.log('n-'+number+' - '+str)
+                    str+=String.fromCharCode(aux);
+                    number++;
+                }else if(aux>58 && letter<4){
+                    console.log('l-'+letter+' - '+str)
+                    str+=String.fromCharCode(aux);
+                    letter++;
+                }
+                console.log(str+' - '+str.length)
+                console.log()
+            }
+            return str;
         },
     },
 });
